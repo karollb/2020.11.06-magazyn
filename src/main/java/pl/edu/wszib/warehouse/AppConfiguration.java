a01e51230e8c1117aab9bc6236c0ecae4ad0bcd6
kasgos <karol.baran@interia.pl>
2020-11-19 17:18:09 +0100

    Fourth Commit

--- src/main/java/pl/edu/wszib/warehouse/AppConfiguration.java
+++ src/main/java/pl/edu/wszib/warehouse/AppConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import pl.edu.wszib.warehouse.database.DataBase;
 import pl.edu.wszib.warehouse.database.IDataBase;
 import pl.edu.wszib.warehouse.gui.GUI;
-import pl.edu.wszib.warehouse.gui.GUI2;
 import pl.edu.wszib.warehouse.gui.IGUI;
 
 @Configuration
